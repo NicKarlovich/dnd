0e6eca71b985d0c96edfbf5ffcf7ea876c4a72cf
NicKarlovich <nick4pie@gmail.com>
2023-10-24 01:03:58 -0500

    try 17 at getting images to show up

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/dnd'
+    basePath: '/dnd',
+    images: {
+        // domains: ...,
+        path: `dnd/_next/image`,
+      }
 }
+
 
 module.exports = nextConfig
