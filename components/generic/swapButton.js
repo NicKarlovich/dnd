5fc7e9ea8ebf7aa1f58b621d433e092190226b39
NicKarlovich <nick4pie@gmail.com>
2023-11-03 01:16:33 -0500

    try 1 at fixing swapNotRotating error on mobile

--- components/generic/swapButton.js
+++ components/generic/swapButton.js
@@ -21,6 +21,7 @@ export default function SwapButton({scale, onAbilitySwap, abilitySwap, index}) {
                 transform: "rotate(180deg)",
                 fontSize: `${scale * 32}px`,
                 writingMode: "vertical-rl",
+                WebkitWritingMode: "vertical-rl",
                 borderLeft: "2px solid grey", //called border left but appears on right because we rotated button 180 so writingMode would work properly
                 background: `${selected}`
                 }}
